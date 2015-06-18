f37cc2b95a89b726f790bee9a1127f5a734dc195
Matthew Setter <matthew.setter@gmail.com>
2015-06-18 17:31:13 +0200

    First commit to the project
    This adds the core work required for a working site

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+date_default_timezone_set('Europe/Berlin');
+
+use Slim\Slim;
+
+// Initialise a Slim app
+$app = new Slim(array(
+    'debug' => true,
+    'mode' => 'development',
+    'view' => new \Slim\Views\Twig(),
+    'templates.path' => dirname(__FILE__) . '/../storage/templates'
+));
+
+// Setup the app views
+$view = $app->view();
+
+$view->parserOptions = array(
+    'debug' => true,
+    'cache' => dirname(__FILE__) . '/../storage/cache'
+);
+
+/**
+ * List all calls on the account
+ */
+$app->get('/', function () use ($app) {
+    $app->render(
+        'home.twig', []
+    );
+});
+
+$app->run();
