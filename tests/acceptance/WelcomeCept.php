0672aa0d91b8ae2632730a092eef80e959d1b3d0
Matthew Setter <matthew.setter@gmail.com>
2015-06-21 22:01:30 +0200

    Add codeception support and the first set of tests
    The tests aren't that intense, but they work, and provide basic coverage
    for the application.

---
+++ tests/acceptance/WelcomeCept.php
@@ -0,0 +1,22 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->wantTo('ensure that frontpage works');
+$I->amOnPage('/');
+$I->seeInTitle('Free The Geek.fm - The podcast about the business of freelancing as a software developer, writer, content producer, and all round PHP lover.');
+// Test the main section
+$I->see('Free Your Inner Geek');
+$I->see('Find out more');
+$I->see('The podcast about the business of freelancing as a software developer, writer, content producer, and all round PHP lover.');
+// Test the "about" section
+$I->see("What's It About?");
+$I->see("It's a fireside chat on the ins and outs of being a freelance writer, screencaster, teacher, and software developer. Looking at what it’s like to do it, warts and all, as well as how to get started, what to expect, the good, the bad, and the ugly. When we’re not talking that, we’re talking tech, code, apps, development, and of course PHP.");
+$I->see("Subscribe!");
+// Test the "contact" section
+$I->see('Got Questions');
+$I->see("If you want to get in touch with me you'll, find all the contact details you need below");
+$I->see("settermjd (skype)");
+$I->see("matthew@freethegeek.fm");
+// Test the links
+$I->seeLink("Free The Geek.fm", "#page-top");
+$I->seeLink("About", "#about");
+$I->seeLink("Contact", "#contact");
