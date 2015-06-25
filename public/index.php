08c3c4fe8c3a9fa42caad9f2322126a4dd128823
Matthew Setter <matthew.setter@gmail.com>
2015-06-25 20:09:06 +0200

    Tidy the bootstrap file up a bit

--- public/index.php
+++ public/index.php
@@ -10,10 +10,19 @@ use Mni\FrontYAML\Parser;
 use Zend\Cache\StorageFactory;
 use Zend\Config\Config;
 
-// Create a cache object, via factory:
+
+// Initialise a Slim app
+$app = new Slim(array(
+    'debug' => true,
+    'mode' => 'development',
+    'view' => new \Slim\Views\Twig(),
+    'templates.path' => dirname(__FILE__) . '/../storage/templates'
+));
 
 // Add Middleware
 $app->add(new \PodcastSite\Middleware\Analytics\GoogleAnalytics());
+
+// Add Cache support
 $cache = StorageFactory::factory(array(
     'adapter' => array(
         'name'    => 'filesystem',
@@ -27,22 +36,14 @@ $cache = StorageFactory::factory(array(
         'serializer'
     ),
 ));
-
-
-// Initialise a Slim app
-$app = new Slim(array(
-    'debug' => true,
-    'mode' => 'development',
-    'view' => new \Slim\Views\Twig(),
-    'templates.path' => dirname(__FILE__) . '/../storage/templates'
-));
-
 $app->cache = $cache;
 
 $config = new Config(
     require_once(dirname(__FILE__) . '/../config/application.php')
 );
 $app->config = $config;
+
+// Add Episode Lister support
 $app->episodeLister = EpisodeLister::factory([
     'type' => 'filesystem',
     'path' => dirname(__FILE__) . '/../storage/posts',
@@ -52,7 +53,6 @@ $app->episodeLister = EpisodeLister::factory([
 
 // Setup the app views
 $view = $app->view();
-
 $view->parserOptions = array(
     'debug' => true,
     'cache' => dirname(__FILE__) . '/../storage/cache/template-cache'
@@ -61,6 +61,7 @@ $view->parserOptions = array(
 // Add in the routes for the application
 require_once('routes.php');
 
+// Add support for 404's and 500's.
 $app->notFound(function () use ($app) {
     $app->render('404.twig');
 });
@@ -69,4 +70,5 @@ $app->error(function (\Exception $e) use ($app) {
     $app->render('500.twig');
 });
 
+// Launch the application
 $app->run();
