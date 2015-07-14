84fd4b2f22837d56ba7c4122e6685103f226a32d
Matthew Setter <matthew.setter@gmail.com>
2015-07-14 08:47:30 +0200

    Updated tests for newer design

--- tests/acceptance/WelcomeCept.php
+++ tests/acceptance/WelcomeCept.php
@@ -3,10 +3,6 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that frontpage works');
 $I->amOnPage('/');
 $I->seeInTitle('Free The Geek.fm - The podcast about the business of freelancing as a software developer, writer, content producer, and all round PHP lover.');
-// Test the main section
-$I->see('Free Your Inner Geek');
-$I->see('Find out more');
-$I->see('The podcast about the business of freelancing as a software developer, writer, content producer, and all round PHP lover.');
 // Test the "about" section
 $I->see("What's It About?");
 $I->see("It's a fireside chat on the ins and outs of being a freelance writer, screencaster, teacher, and software developer. Looking at what it’s like to do it, warts and all, as well as how to get started, what to expect, the good, the bad, and the ugly. When we’re not talking that, we’re talking tech, code, apps, development, and of course PHP.");
@@ -17,6 +13,7 @@ $I->see("If you want to get in touch with me you'll, find all the contact detail
 $I->see("settermjd (skype)");
 $I->see("matthew@freethegeek.fm");
 // Test the links
-$I->seeLink("Free The Geek.fm", "#page-top");
-$I->seeLink("About", "#about");
-$I->seeLink("Contact", "#contact");
+$I->seeLink("Free The Geek.fm", "/");
+$I->seeLink("About", "/#about");
+$I->seeLink("Contact", "/#contact");
+$I->seeLink("Episodes", "/episodes");
