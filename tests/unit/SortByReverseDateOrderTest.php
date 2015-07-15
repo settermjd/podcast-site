933cb5feb6543629a5d6644206ff4dfc286389b2
Matthew Setter <matthew.setter@gmail.com>
2015-07-15 17:14:51 +0200

    Add simple unit test for the episode sorter
    This is rather simplistic, but does a basic job of testing the
    functionality.

---
+++ tests/unit/SortByReverseDateOrderTest.php
@@ -0,0 +1,35 @@
+<?php
+
+class SortByReverseDateOrderTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+    }
+
+    public function testResultsAreSortedInTheCorrectOrder()
+    {
+        $episodeListing = [
+            new \PodcastSite\Entity\Episode('2013-01-01', '', '', ''),
+            new \PodcastSite\Entity\Episode('2015-01-01', '', '', ''),
+            new \PodcastSite\Entity\Episode('2014-01-01', '', '', ''),
+        ];
+
+        // Sort the records in reverse date order
+        $sorter = new \PodcastSite\Sorter\SortByReverseDateOrder();
+        usort($episodeListing, $sorter);
+
+        /** @var \PodcastSite\Entity\Episode $episode */
+        $episode = array_shift($episodeListing);
+        $this->assertEquals('2015-01-01', $episode->getPublishDate('2015-01-01'));
+
+    }
+}
