992e1738b4c6e76be8d513f6dd00792d711da56e
Matthew Setter <matthew.setter@gmail.com>
2015-07-15 08:56:51 +0200

    Refactor subscribe buttons (and add RSS subscription button)

--- tests/acceptance/WelcomeCept.php
+++ tests/acceptance/WelcomeCept.php
@@ -6,7 +6,9 @@ $I->seeInTitle('Free The Geek.fm - The podcast about the business of freelancing
 // Test the "about" section
 $I->see("What's It About?");
 $I->see("It's a fireside chat on the ins and outs of being a freelance writer, screencaster, teacher, and software developer. Looking at what it’s like to do it, warts and all, as well as how to get started, what to expect, the good, the bad, and the ugly. When we’re not talking that, we’re talking tech, code, apps, development, and of course PHP.");
-$I->see("Subscribe!");
+// Test the subscribe buttons
+$I->seeLink("Subscribe via RSS", "/rss.xml");
+$I->seeLink("Subscribe on iTunes!", "https://itunes.apple.com/de/podcast/free-geek.fm-matthew-setter/id1018923368?l=en&mt=2");
 // Test the "contact" section
 $I->see('Got Questions');
 $I->see("If you want to get in touch with me you'll, find all the contact details you need below");
