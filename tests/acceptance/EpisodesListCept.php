4edddd5afb9f254fc27bb87bdf92381fe00ed5b0
Matthew Setter <matthew@maltblue.com>
2015-08-07 09:41:08 +0200

    Further implemented the new look and feel

--- tests/acceptance/EpisodesListCept.php
+++ tests/acceptance/EpisodesListCept.php
@@ -5,7 +5,3 @@ $I->amOnPage('/episodes');
 $I->seeInTitle('Free The Geek.fm - The podcast about the business of freelancing as a software developer, writer, content producer, and all round PHP lover.');
 $I->see("Episodes", "//h2[@class='section-heading']");
 
-// Test the subscribe buttons
-$I->seeLink("Subscribe via RSS", "/rss.xml");
-$I->seeLink("Subscribe on iTunes!", "https://itunes.apple.com/de/podcast/free-geek.fm-matthew-setter/id1018923368?l=en&mt=2");
-
