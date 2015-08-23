8c074e89d504b3399a9775a0b2a4de3742d8d07f
Matthew Setter <matthew@maltblue.com>
2015-08-23 23:01:32 +0200

    Implement the new RSS route
    This only outputs an iTunes-compliant RSS feed file. The plan is to
    expand it to other feed formats.

--- public/routes.php
+++ public/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use \PodcastSite\Feed\FeedCreatorFactory;
+
 /**
  * The default route
  */
@@ -55,49 +57,11 @@ $app->get('/episode/:episodeSlug', function ($episodeSlug) use ($app) {
 })->name('episode');
 
 $app->get('/rss', function() use ($app) {
-    /**
-     * Create the parent feed
-     */
-    $feed = new Zend\Feed\Writer\Feed;
-    $feed->setTitle('Paddy\'s Blog');
-    $feed->setLink('http://www.freethegeek.fm');
-    $feed->setFeedLink('http://www.freethegeek.com/feed/rss', 'rss');
-    $feed->addAuthor(array(
-        'name'  => 'Matthew Setter',
-        'email' => 'matthew@freethegeek.fm',
-        'uri'   => 'http://www.freethegeek.fm',
-    ));
-    $feed->setDateModified(time());
-    $feed->setDescription("Here's a description");
-
-    /**
-     * Add one or more entries. Note that entries must
-     * be manually added once created.
-     */
-    $entry = $feed->createEntry();
-    $entry->setTitle('All Your Base Are Belong To Us');
-    $entry->setLink('http://www.example.com/all-your-base-are-belong-to-us');
-    $entry->addAuthor(array(
-        'name'  => 'Paddy',
-        'email' => 'paddy@example.com',
-        'uri'   => 'http://www.example.com',
-    ));
-    $entry->setDateModified(time());
-    $entry->setDateCreated(time());
-    $entry->setDescription('Exposing the difficultly of porting games to English.');
-    $entry->setContent(
-        'I am not writing the article. The example is long enough as is ;).'
+    $feedCreator = FeedCreatorFactory::factory('itunes');
+    $feed = $feedCreator->generateFeed(
+        $app->show,
+        $app->episodeLister->getEpisodeList()
     );
-    $feed->addEntry($entry);
-
-    /**
-     * Render the resulting feed to Atom 1.0 and assign to $out.
-     * You can substitute "atom" with "rss" to generate an RSS 2.0 feed.
-     */
-    $app->response()->header('Content-Type', 'application/rss+xml');
-    $app->render('rss-feed.twig');
-
-    $response = $app->response();
-    $response['Content-Type'] = 'application/rss+xml';
-    $response->body($feed->export('rss'));
+    $app->contentType($feed->getContentType());
+    print $feed->generate('rss2');
 })->name('rss/itunes');
