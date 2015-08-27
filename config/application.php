f62ca945b546b11e3d09accea3a42e68e3088ac0
Matthew Setter <matthew@maltblue.com>
2015-08-27 13:13:12 +0200

    Remove non-ascii chars from config

--- config/application.php
+++ config/application.php
@@ -10,7 +10,7 @@ return [
         'episodePrefix' => 'episodes/',
         'title' => 'Free the Geek.fm with Matthew Setter',
         'subtitle' => 'The podcast about the business of freelancing as a software developer, writer, content producer, and all round PHP lover',
-        'description' => 'A fireside chat on the ins and outs of being a freelance writer, screencaster, teacher, and software developer. Looking at what it’s like to do it, warts and all, as well as how to get started, what to expect, the good, the bad, and the ugly. When we’re not talking that, we’re talking tech, code, apps, development, and of course PHP.',
+        'description' => "A fireside chat on the ins and outs of being a freelance writer, screencaster, teacher, and software developer. Looking at what it's like to do it, warts and all, as well as how to get started, what to expect, the good, the bad, and the ugly. When we’re not talking that, we're talking tech, code, apps, development, and of course PHP.",
         'link' => 'http://www.freethegeek.fm',
         'author' => [
             'name' => 'FreetheGeek.fm with Matthew Setter',
