bec9ce969fb42e370b3e75eb935a219a8906ae10
Matthew Setter <matthew@maltblue.com>
2015-10-24 20:30:03 +0200

    Add nyan formatter, codeception verify and specify modules

---
+++ bin/run-tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+##
+## This is just a simple script to make running tests easy, and consistent.
+## Feel free to improve it.
+##
+vendor/bin/codecept run  --coverage --coverage-xml --coverage-html --coverage clover
