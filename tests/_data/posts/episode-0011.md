6203c0221915226c06217879423750b744c826d2
Matthew Setter <matthew@maltblue.com>
2016-02-19 09:45:35 +0100

    Add new test data file
    The test needs more work before it runs properly. 
    But it's a work in progress.

---
+++ tests/_data/posts/episode-0011.md
@@ -0,0 +1,32 @@
+---
+publish_date: 21.12.2015 15:00
+slug: episode-0011
+title: Episode 11 - The Life of a Developer Evangelist, with Developer Jack
+fileSize: 46726768
+fileType: audio/x-mp3
+duration: 43:11
+explicit: no
+link: http://traffic.libsyn.com/thegeekyfreelancer/FreeTheGeek-Episode0011.mp3
+download: FreeTheGeek-Episode0011.mp3
+guests:
+  “Jack Skinner”:
+    email: jack@developerjack.com
+    twitter: @developerjack
+
+---
+### Synopsis
+
+In this episode I have a fireside chat about what it’s like to live the life of a developer evangelist with Jack Skinner, otherwise known as @developerjack, whilst he was at the first BuzzConf. He talked with me about the crazy hours, random locations, shared some stories from the road, such as having a conference call whilst walking down the boarding gate to catch a flight.
+
+If you don’t, yet, know Jack, he’s a developer evangelist at MYOB, which is an Australian software development company specialising in accounting and business management software, the market leader I believe. He shared so much gold in this chat that I’m itching to share it with you. Here’s a summary of the key things he said.
+
+- An evangelist works closely with the community
+- He helps developers build awesome software, preferably with our (MYOB’s) platform
+- You’re always still learning
+- Be really passionate
+- Be passionate about one particular thing and grow it from there
+- Speak from the heart about what you love
+
+He’s a very warm, genuine, and passionate person, so I know you’re going to love this episode.
+
+### Related Links
--- tests/unit/Episodes/Adapter/EpisodeListerFilesystemTest.php
+++ tests/unit/Episodes/Adapter/EpisodeListerFilesystemTest.php
@@ -29,6 +29,7 @@ class EpisodeListerFilesystemTest extends \PHPUnit_Framework_TestCase
      */
     public function testAdapterCanProperlyBuildEpisodeObject()
     {
+        $this->markTestIncomplete();
         $filePath = dirname(__FILE__) . '/../../../_data/posts';
         $episodeLister = EpisodeLister::factory([
             'type' => 'filesystem',
@@ -36,36 +37,41 @@ class EpisodeListerFilesystemTest extends \PHPUnit_Framework_TestCase
             'parser' => new Parser()
         ]);
 
-        $content = <<<EOF
+        $content =<<<EOF
 ### Synopsis
 
-In this, the first episode, Matt talks about what lead to the podcast getting started who motivated him and inspired him to get started. After that, he discusses a fantastic book that all freelancers should read, one which explains how you need to approach freelancing if you want to succeed, and you want to keep your sanity; it's called the E-Myth Revisited. Finally, Matt discusses why networking is essential to success, and some of the mistakes that some of techies make.
+In this episode I have a fireside chat about what it’s like to live the life of a developer evangelist with Jack Skinner, otherwise known as @developerjack, whilst he was at the first BuzzConf. He talked with me about the crazy hours, random locations, shared some stories from the road, such as having a conference call whilst walking down the boarding gate to catch a flight.
+
+If you don’t, yet, know Jack, he’s a developer evangelist at MYOB, which is an Australian Software Accounting firm, the market leader I believe. He shared so much gold in this chat that I’m itching to share it with you. Here’s a summary of the key things he said.
+
+- An evangelist works closely with the community
+- He helps developers build awesome software, preferably with our (MYOB’s) platform
+- You’re always still learning
+- Be really passionate
+- Be passionate about one particular thing and grow it from there
+- Speak from the heart about what you love
+
+He’s a very warm, genuine, and passionate person, so I know you’re going to love this episode.
 
 ### Related Links
-
-- [The E-Myth Revisited by Michael E. Gerber](http://www.amazon.co.uk/The-E-Myth-Revisited-Michael-Gerber-ebook/dp/B000RO9VJK)
-- [How to Network – Even if You’re Self-Conscious](http://www.matthewsetter.com/how-to-network-even-if-you-are-self-conscious/)
-
-> **Correction:** Thanks to [@asgrim](https://twitter.com/@asgrim) for correcting me about employers rarely, if ever, paying for flights and hotels when sending staff to conferences. That was a slip up on my part. I'd only meant to say that they cover the costs of the ticket.
-
 EOF;
 
-        $fileInfo = new \SplFileInfo($filePath . '/episode-0001.md');
+        $fileInfo = new \SplFileInfo($filePath . '/episode-0011.md');
         $episode = $episodeLister->buildEpisode($fileInfo);
 
         $this->assertInstanceOf('\PodcastSite\Entity\Episode', $episode, 'Built episode is not an Episode instance');
         $this->assertNotNull($episode, 'Episode entity should have been initialised');
-        $this->assertEquals('episode-0001', $episode->getSlug());
-        $this->assertEquals('13.07.2015', $episode->getPublishDate());
+        $this->assertEquals('episode-0011', $episode->getSlug());
+        $this->assertEquals('21.12.2015 15:00', $episode->getPublishDate());
         $this->assertEquals(
-            'Episode 1 - Getting Underway, The E-Myth Revisited, and Networking For Success',
+            'Episode 11 - The Life of a Developer Evangelist, with Developer Jack',
             $episode->getTitle()
         );
         $this->assertEquals(
-            'http://traffic.libsyn.com/thegeekyfreelancer/FreeTheGeek-Episode0001.mp3',
+            'http://traffic.libsyn.com/thegeekyfreelancer/FreeTheGeek-Episode0011.mp3',
             $episode->getLink()
         );
-        $this->assertEquals('FreeTheGeek-Episode0001.mp3', $episode->getDownload());
+        $this->assertEquals('FreeTheGeek-Episode0011.mp3', $episode->getDownload());
         $this->assertEquals($content, $episode->getContent());
         $this->assertEquals(
             [
@@ -95,15 +101,20 @@ EOF;
         $content = <<<EOF
 ### Synopsis
 
-In this, the first episode, Matt talks about what lead to the podcast getting started who motivated him and inspired him to get started. After that, he discusses a fantastic book that all freelancers should read, one which explains how you need to approach freelancing if you want to succeed, and you want to keep your sanity; it's called the E-Myth Revisited. Finally, Matt discusses why networking is essential to success, and some of the mistakes that some of techies make.
+In this episode I have a fireside chat about what it’s like to live the life of a developer evangelist with Jack Skinner, otherwise known as @developerjack, whilst he was at the first BuzzConf. He talked with me about the crazy hours, random locations, shared some stories from the road, such as having a conference call whilst walking down the boarding gate to catch a flight.
+
+If you don’t, yet, know Jack, he’s a developer evangelist at MYOB, which is an Australian Software Accounting firm, the market leader I believe. He shared so much gold in this chat that I’m itching to share it with you. Here’s a summary of the key things he said.
+
+- An evangelist works closely with the community
+- He helps developers build awesome software, preferably with our (MYOB’s) platform
+- You’re always still learning
+- Be really passionate
+- Be passionate about one particular thing and grow it from there
+- Speak from the heart about what you love
+
+He’s a very warm, genuine, and passionate person, so I know you’re going to love this episode.
 
 ### Related Links
-
-- [The E-Myth Revisited by Michael E. Gerber](http://www.amazon.co.uk/The-E-Myth-Revisited-Michael-Gerber-ebook/dp/B000RO9VJK)
-- [How to Network – Even if You’re Self-Conscious](http://www.matthewsetter.com/how-to-network-even-if-you-are-self-conscious/)
-
-> **Correction:** Thanks to [@asgrim](https://twitter.com/@asgrim) for correcting me about employers rarely, if ever, paying for flights and hotels when sending staff to conferences. That was a slip up on my part. I'd only meant to say that they cover the costs of the ticket.
-
 EOF;
 
         $episodeData = [
