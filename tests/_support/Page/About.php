0606f6fb54cf25c718d95ae797900d580d41762d
Matthew Setter <matthew@maltblue.com>
2016-02-23 14:14:10 +0100

    Refactor About page acceptance test to use page objects

---
+++ tests/_support/Page/About.php
@@ -0,0 +1,34 @@
+<?php
+namespace Page;
+
+class About
+{
+    // include url of current page
+    public static $URL = '/about';
+
+    /**
+     * @var AcceptanceTester
+     */
+    protected $tester;
+
+    public function __construct(\AcceptanceTester $I)
+    {
+        $this->tester = $I;
+    }
+
+    public function checkAboutPage()
+    {
+        $I = $this->tester;
+
+        $I->amOnPage('/about');
+        $I->seeInTitle('Free the Geek.fm with Matthew Setter - The podcast about the business of freelancing as a software developer, writer, content producer, and all round PHP lover');
+
+        $I->seeLink("About", "/about");
+        $I->seeLink("Contact", "/contact");
+
+        $I->see("In a Nutshell");
+        $I->see("Why Did I Launch It?");
+        $I->see("Here's What You Get");
+        $I->see("My Promise To You");
+    }
+}
