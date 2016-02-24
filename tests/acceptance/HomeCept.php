c01b9d6f8314ecd7a6e706afe7c4102702d6241c
Matthew Setter <matthew@maltblue.com>
2016-02-24 07:46:10 +0100

    Refactor home page test to use the PageObject pattern

--- tests/acceptance/HomeCept.php
+++ tests/acceptance/HomeCept.php
@@ -2,24 +2,7 @@
 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that frontpage works');
-$I->amOnPage('/');
-$I->seeInTitle('Free the Geek.fm with Matthew Setter - The podcast about the business of freelancing as a software developer, writer, content producer, and all round PHP lover.');
 
-// Test the links
-$I->seeLink("About", "/about");
-$I->seeLink("Contact", "/contact");
+$page = new \Page\Home($I);
 
-$I->see("Latest Episode");
-$I->see("Upcoming Episodes");
-$I->see("Past Episodes");
-
-// Test the subscribe buttons
-$I->seeLink('', "https://twitter.com/@freeingthegeek");
-$I->seeLink('', "https://plus.google.com/b/115002379460010233732/115002379460010233732/posts");
-$I->seeLink('', "https://www.facebook.com/pages/Free-The-Geek/899450083436065");
-$I->seeLink('', "http://freethegeek.fm/rss.xml");
-$I->seeLink('', "https://itunes.apple.com/podcast/free-geek.fm-matthew-setter/id1018923368?l=en&mt=2");
-
-$I->seeLink("Matthew Setter", "http://www.matthewsetter.com");
-$I->seeLink("the Slim Framework", "http://slimframework.com");
-$I->dontSeeElement("//div[@id='upcoming-episodes']//h3[@class='media-heading']/a");
+$page->testHomePage();
