79167efeca2990808a188f3141cf54a170068cd3
Matthew Setter <matthew@maltblue.com>
2016-02-24 07:50:43 +0100

    Fix up test for redirect to iTunes

--- tests/acceptance/iTunesCept.php
+++ tests/acceptance/iTunesCept.php
@@ -3,5 +3,4 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('Ensure that the redirect to the iTunes page works');
 $I->amOnPage('/itunes');
-$I->seeResponseCodeIs(302);
-$I->seeHttpHeader('Location', 'https://itunes.apple.com/podcast/free-geek.fm-matthew-setter/id1018923368?l=en&mt=2');
+$I->seeInCurrentUrl('/podcast/free-geek.fm-matthew-setter/id1018923368?l=en&mt=2');
