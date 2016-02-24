9862d5f63dada2423bc02f651b059262dcb84887
Matthew Setter <matthew@maltblue.com>
2016-02-24 07:43:25 +0100

    Refactor Contact page test in to a PageObject

---
+++ tests/_support/Page/Contact.php
@@ -0,0 +1,34 @@
+<?php
+namespace Page;
+
+class Contact
+{
+    /**
+     * @var string
+     */
+    public static $URL = '/contact';
+
+    /**
+     * @var AcceptanceTester
+     */
+    protected $tester;
+
+    public function __construct(\AcceptanceTester $I)
+    {
+        $this->tester = $I;
+    }
+
+    public function testContactPage()
+    {
+        $I = $this->tester;
+
+        $I->amOnPage('/contact');
+        $I->seeInTitle('Free the Geek.fm with Matthew Setter - The podcast about the business of freelancing as a software developer, writer, content producer, and all round PHP lover');
+
+        $I->seeLink("About", "/about");
+        $I->seeLink("Contact", "/contact");
+
+        $I->see("Got Questions");
+        $I->see("If you want to get in touch with me, whether it's feedback about the show, the site, or something completely different, I'm available via the following options");
+    }
+}
--- tests/acceptance/ContactPageCept.php
+++ tests/acceptance/ContactPageCept.php
@@ -2,12 +2,6 @@
 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that contact page works');
-$I->amOnPage('/contact');
-$I->seeInTitle('Free the Geek.fm with Matthew Setter - The podcast about the business of freelancing as a software developer, writer, content producer, and all round PHP lover');
 
-// Test the links
-$I->seeLink("About", "/about");
-$I->seeLink("Contact", "/contact");
-
-$I->see("Got Questions");
-$I->see("If you want to get in touch with me, whether it's feedback about the show, the site, or something completely different, I'm available via the following options");
+$page = new \Page\Contact($I);
+$page->testContactPage();
