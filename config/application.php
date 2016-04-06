96a482d78c707c934a4f88ba03fcc34d82994197
Matthew Setter <matthew@maltblue.com>
2016-04-06 21:06:58 +0200

    Update site title and category

--- config/application.php
+++ config/application.php
@@ -9,8 +9,8 @@ return [
         'url' => 'http://freethegeek.fm/',
         'episodePrefix' => 'episode/',
         'title' => 'Free the Geek.fm with Matthew Setter',
-        'subtitle' => 'The podcast about the business of freelancing as a software developer, writer, content producer, and all round PHP lover',
-        'description' => "A fireside chat on the ins and outs of being a freelance writer, screencaster, teacher, and software developer. Looking at what it's like to do it, warts and all, as well as how to get started, what to expect, the good, the bad, and the ugly. When we're not talking that, we're talking tech, code, apps, development, and of course PHP.",
+        'subtitle' => 'The podcast about building a rewarding career as a professional software developer.',
+        'description' => "A fortnightly, fireside chat, on the ins and outs of building a rewarding career as a professional software developer. Looking at what it's like to do it, warts and all, as well as how to get started, what to expect, the good, the bad, and the ugly.",
         'link' => 'http://www.freethegeek.fm',
         'author' => [
             'name' => 'FreetheGeek.fm with Matthew Setter',
@@ -20,7 +20,7 @@ return [
             'Freelancing'
         ],
         'category' => 'Technology',
-        'subcategory' => 'Podcasting',
+        'subcategory' => 'Software How-To',
         'artwork' => 'http://traffic.libsyn.com/thegeekyfreelancer/itunes.png',
         'explicit' => false
     ]
